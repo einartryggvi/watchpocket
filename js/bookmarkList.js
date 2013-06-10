4300a1d66f88223f13e2c1c6210d33944c234bda
Einar Tryggvi Leifsson <einartryggvi@gmail.com>
2013-06-10 23:00:41 +0000

    Fix.

--- js/bookmarkList.js
+++ js/bookmarkList.js
@@ -1,10 +1,5 @@
 $(function() {
-	if (!watchpocket.isLoggedIn()) {
-		document.location.href = watchpocket.loginUrl;
-	}
-	else {
-		watchpocket.loadBookmarks('#bookmarks tbody');
-	}
+	watchpocket.loadBookmarks('#bookmarks tbody');
 	$('#bookmarks').on('click', 'tr', function() {
 		chrome.tabs.create({url: $(this).data('url')});
 	});
